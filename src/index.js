f4f949b15953e42b9d38d72675d6cf823be431cc
Sergei Anisenkov <s.anisenkov@volsor.com>
2020-08-11 16:31:44 +0200

    fix height

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import 'reset-css'
 import './css/styles.css'
 import './css/responsive.css'
-import './js/get_height.js'
 import './js/add_avatar.js'
 
 import loanProfile from './js/profile.js'
 import loanFriends from './js/friends.js'
 import loanPosts from './js/posts.js'
+import addHeight from './js/get_height.js'
 
 window.onload = function () {
     loanProfile;
     loanFriends;
     loanPosts;
+    addHeight;
 }
